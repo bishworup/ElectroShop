7f3f6013f7012cb332e8be58bc04a6c7d992ec0a
bishworup <Bshwo890@gmail.com>
2020-09-05 10:21:52 +0545

    Created login class and node response class

---
+++ app/src/main/java/com/bishworup/electroshop/Response/NodeResponse.java
@@ -0,0 +1,27 @@
+package com.bishworup.electroshop.Response;
+
+public class NodeResponse {
+    private String status;
+    private String token;
+
+    public NodeResponse(String status, String token) {
+        this.status = status;
+        this.token = token;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
