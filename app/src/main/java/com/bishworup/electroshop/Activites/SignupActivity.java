ef03b1c663e10bfd02f17e1fa82f3c4462c0778a
bishworup <Bshwo890@gmail.com>
2020-09-05 10:19:05 +0545

    Login and signup design created

--- app/src/main/java/com/bishworup/electroshop/Activites/SignupActivity.java
+++ app/src/main/java/com/bishworup/electroshop/Activites/SignupActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.bishworup.electroshop.R;
+
 public class SignupActivity extends AppCompatActivity {
 
     @Override
